a70d66db6de9d92b4f9d2630f44e2b99e7c19239
Charlie Suarez Robles <charlie.suarez.robles@gmail.com>
2025-11-14 22:47:23 -0800

    I added another unit test for the CI pipeline inside of the user.unit.test.ts file, fixed the first unit test and added a block of code after the imports

--- backend/test/controllers/user.unit.test.ts
+++ backend/test/controllers/user.unit.test.ts
@@ -1,11 +1,14 @@
 import { describe, it, expect, vi } from "vitest";
 import { signup, login } from "../../src/controllers/user";
+import { getUserByUsername } from "../../src/data/user_db"
+
+vi.mock("../../src/data/user_db", () => ({
+  getUserByUsername: vi.fn(),
+}));
 
 describe("Signup Tests", () => {
-    it("It should not succeed if the username or the password are incorrect", async () => {
-        const username = "Amie";
-        const password = "1234";
-        const req = { body: { username: username, password: password } } as any;
+    it("It should not succeed if the username or the password are null", async () => {
+        const req = { body: {} } as any;
         const res = {
             status: vi.fn().mockReturnThis(),
             json: vi.fn(),
@@ -13,9 +16,31 @@ describe("Signup Tests", () => {
 
         await signup(req, res);
 
-        expect(res.status).toHaveBeenCalledWith(201);
+        expect(res.status).toHaveBeenCalledWith(400);
         expect(res.json).toHaveBeenCalledWith({
             error: "username and password are required",
+        });
+    });
+      it("returns 409 if username already exists", async () => {
+        (getUserByUsername as any).mockResolvedValueOnce({
+        id: 1,
+        username: "FirstUser"
+        });
+
+        const req = {
+            body: { username: "FirstUser", password: "12345" }
+        } as any;
+
+        const res = {
+            status: vi.fn().mockReturnThis(),
+            json: vi.fn()
+        } as any;
+
+        await signup(req, res);
+
+        expect(res.status).toHaveBeenCalledWith(409);
+        expect(res.json).toHaveBeenCalledWith({
+        error: "Username already exists"
         });
     });
 });

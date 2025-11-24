efd95d4c4a80d2223d4e2559e2daf0917fa8e68d
Charlie Suarez Robles <charlie.suarez.robles@gmail.com>
2025-11-23 21:34:22 -0800

    Created all the three file views necessary for the game

---
+++ frontend/src/screens/Minigame2Screen/Minigame2ScreenViewEntrance2.ts
@@ -0,0 +1,8 @@
+import Konva from "konva";
+import type { View } from "../../types";
+import { CONTENT_HEIGHT, CONTENT_WIDTH, STAGE_HEIGHT, STAGE_WIDTH } from "../../constants";
+import type { Question } from "../../types";
+
+export class Minigame2Entrance2ScreenView implements View {
+
+}
